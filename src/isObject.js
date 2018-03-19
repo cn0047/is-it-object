b0597b119f4b9aa6708052b5eb857675fc7bfecd
Vladimir Kovpak <cn007b@gmail.com>
2018-03-19 15:08:11 +0200

    Refactored isObject.js

--- src/isObject.js
+++ src/isObject.js
@@ -1,28 +1,37 @@
 /**
+ * Check that value is an object using strict strategy.
+ *
+ * @param o Value for inspection is it an object.
+ * @returns {boolean} Is object.
+ */
+function checkByStrictStrategy(o) {
+  return (o instanceof Object || typeof o === 'object')
+    && (o.constructor === undefined || o.constructor === Object);
+}
+
+/**
+ * Check that value is an object using weak (non-strict) strategy.
+ *
+ * @param o Value for inspection is it an object.
+ * @returns {boolean} Is object.
+ */
+function checkByWeakStrategy(o) {
+  return o.constructor === undefined || typeof o.constructor === 'function';
+}
+
+/**
  * Check that value is an object.
  *
  * @param o Value for inspection is it an object.
  * @param {boolean} strict Strict comparison strategy.
  * @returns {boolean} Is object.
  */
-module.exports = function isObject(o, strict = true) {
+function isObject(o, strict = true) {
   if (o === null || o === undefined) {
     return false;
   }
 
-  const instanceOfObject = o instanceof Object;
-  const typeOfObject = typeof o === 'object';
-  const constructorUndefined = o.constructor === undefined;
-  const constructorObject = o.constructor === Object;
-  const typeOfConstructorObject = typeof o.constructor === 'function';
+  return strict === true ? checkByStrictStrategy(o) : checkByWeakStrategy(o);
+}
 
-  let r;
-
-  if (strict === true) {
-    r = (instanceOfObject || typeOfObject) && (constructorUndefined || constructorObject);
-  } else {
-    r = (constructorUndefined || typeOfConstructorObject);
-  }
-
-  return r;
-};
+module.exports = isObject;
